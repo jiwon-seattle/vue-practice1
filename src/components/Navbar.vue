f1e4f44d1b9f1f92376c4c7b7c4b0ff95779ad72
Jiwon Han <jiwon1.han@gmail.com>
2020-11-01 20:15:00 -0800

    prac for ceo dashboard

---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+    <nav>
+        <v-navigation-drawer v-model="drawer" dark app mini-variant class="pink accent-2">
+            <v-list tile>
+                <v-list-item class="mb-8" router to ="/">
+                    <v-list-item-action>
+                        <v-icon>fas fa-window-restore</v-icon>
+                    </v-list-item-action>
+                </v-list-item>
+                <v-list-item class="mb-8">
+                    <v-list-item-action>
+                        <v-icon>far fa-clock</v-icon>
+                    </v-list-item-action>
+                </v-list-item>
+                <v-list-item class="mb-8">
+                    <v-list-item-action>
+                        <v-icon>fas fa-chart-line</v-icon>
+                    </v-list-item-action>
+                </v-list-item>
+                <v-list-item class="mb-8">
+                    <v-list-item-action>
+                        <v-icon>far fa-comment-alt</v-icon>
+                    </v-list-item-action>
+                </v-list-item>
+                <v-list-item class="mb-8">
+                    <v-list-item-action>
+                        <v-icon>fas fa-cog</v-icon>
+                    </v-list-item-action>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-navigation-drawer v-model="right" dark app class="deep-orange accent-2" fixed right>
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/img1.png" alt="">
+                    </v-avatar>
+                    <h4 class="white--text subheading mt-1 text-center">Jiwon Han</h4>
+                    <h6 class="white--text subheading mt-1 text-center">CEO</h6>
+                </v-flex>
+            </v-layout>
+            <p class="white--text subheadding pl-3 mt-3">Rank of Company</p>
+            <v-list rounded dense>
+                <v-list-item router to="/">
+                    <v-list-item-action>
+                        03
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>The Income</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-action>
+                        05
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Human Resources</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-action>
+                        12
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>The Growth</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <p class="white--text subheadding pl-3 mt-3">Next Meeting Places</p>
+            <v-timeline align-top dense fill-dot small dark>
+                <v-timeline-item fill-dot small color="white" class="white--text">Seoul</v-timeline-item>
+                <v-timeline-item fill-dot small color="white" class="white--text">Tokyo</v-timeline-item>
+                <v-timeline-item fill-dot small color="white" class="white--text">San Francisco</v-timeline-item>
+            </v-timeline>
+        </v-navigation-drawer>
+    </nav>
+</template>
